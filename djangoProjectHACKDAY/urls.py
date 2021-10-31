bacc9e420ed1821b8e26b7902b5c5f625cdff734
Stormtrooperroman <44937524+Stormtrooperroman@users.noreply.github.com>
2021-10-31 15:47:04 +0700

    Commit

--- djangoProjectHACKDAY/urls.py
+++ djangoProjectHACKDAY/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import calc.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('buy', views.buy),
+    path('show', views.show)
 ]
