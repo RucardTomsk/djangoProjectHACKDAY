22dfdbe2446e2fbb4284f554f6b324da24384910
Stormtrooperroman <44937524+Stormtrooperroman@users.noreply.github.com>
2021-10-31 08:26:26 +0700

    Add Animation

--- front/script.js
+++ front/script.js
@@ -1,23 +1,48 @@
-'use strict'
-var form = document.getElementById("form");
-form.addEventListener('submit', function (e) {
+function stopAnim(){
+    $(".animationHeader").fadeOut( 1600, "linear" );
+    $("#content").fadeIn(1600, "linear");
+}
 
-    const regex = new RegExp('^[0-9]+$');
-    var number = $("#coll").val();
-    
-    if(!form.checkValidity())
-    {
+setTimeout(function() { stopAnim(); }, 5000);
+
+
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+    form.addEventListener('submit', function (e) {
         e.preventDefault()
         e.stopPropagation()
-        
-    }
-    form.classList.add('was-validated')
-    if(regex.test(number)){
-        console.log("Ok");
-    }
-    else{
-        e.preventDefault()
-        e.stopPropagation()
-    }
+        const regex = new RegExp('^[0-9]+$');
+        var collHave = $("#collHave").val();
+        var collSave = $("#collSave").val();
+        $(".invalid-back").css("display", "none");
+        if (!form.checkValidity()) {
+            
+        }
 
-});
+        form.classList.add('was-validated')
+
+        if(regex.test(collHave)){
+            if(regex.test(collSave)){
+                
+            }
+            else{
+                document.getElementById("collSave").setCustomValidity('invalid')
+            }
+        }
+        else{
+            document.getElementById("collHave").setCustomValidity('invalid')
+        }
+    }, false)
+    })
+})()
+
+
+
+
